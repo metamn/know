be8c40d813e2d7685b53d07d2904926fb43bee07
Oscar Del Ben <oscar@iMac-6.local>
2009-12-07 18:39:18 +0100

    views

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -4,4 +4,16 @@ class ChannelsController < ApplicationController
     @channels = Channel.all(:include => :sources)
   end
   
+  def new
+    @channel = Channel.new
+  end
+  
+  def create
+    @channel = Channel.new(params[:channel])
+    @channel.save!
+    redirect_to channels_url
+  rescue ActiveRecord::RecordInvalid
+    render :action => :new
+  end
+  
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,4 +2,6 @@ class Channel < ActiveRecord::Base
   
   has_many :sources
   
+  validates_presence_of :name
+  
 end
